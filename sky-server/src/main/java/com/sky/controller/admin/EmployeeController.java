ca78c2fa2babc6cc48f4e1785b212e7db92adbd9
liyian-315 <859679381@qq.com>
2024-08-23 16:17:36 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -103,4 +103,19 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+    /**
+     * 启用禁用员工账号
+     * 路径参数加@PathVariable注解获取
+     * 地址栏参数id直接获取，只要名称一致就可以
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号:{}，{}",status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 }
